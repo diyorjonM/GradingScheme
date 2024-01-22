ae1f89cbce15b0c23aa4835e70cc3a193226f331
yxngdior <dmavlonkulov@gmail.com>
2024-01-21 22:02:03 -0500

    Initial Commit

---
+++ GradingScheme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GradingScheme
+//
+//  Created by Diyorjon Mavlonkulov on 1/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
